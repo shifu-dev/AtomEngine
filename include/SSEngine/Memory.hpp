f0da9820bfbbb2e8f6f0082fa05086dcabcb6a72
shifu-dev <csingh8476@gmail.com>
2022-10-12 22:01:10 +0530

    Added HeapMemoryPool

--- include/SSEngine/Memory.hpp
+++ include/SSEngine/Memory.hpp
@@ -3,4 +3,5 @@
 #include "SSEngine/Memory/MemPool.hpp"
 #include "SSEngine/Memory/LinkedMemPool.hpp"
 #include "SSEngine/Memory/StackMemPool.hpp"
+#include "SSEngine/Memory/HeapMemPool.hpp"
 #include "SSEngine/Memory/Pointers.hpp"
---
+++ include/SSEngine/Memory/HeapMemPool.hpp
@@ -0,0 +1,56 @@
+#pragma once
+#include "SSEngine/Core.hpp"
+#include "SSEngine/Memory/LinkedMemPool.hpp"
+
+namespace SSEngine
+{
+    class HeapMemPool : public virtual DynamicLinkedMemPool
+    {
+        using LinkedMemPool::mFirstBlock;
+        using LinkedMemPool::mFreeBlock;
+
+    public:
+        HeapMemPool(const sizet size) noexcept;
+
+        virtual sizet Size() const noexcept override;
+
+        virtual void Resize(const sizet size) override;
+
+        virtual void Reserve(const sizet size) override;
+
+    protected:
+        sizet mSize;
+    };
+
+    HeapMemPool::HeapMemPool(const sizet size) noexcept
+    {
+        void ptr heapMem = malloc(size);
+
+        mFirstBlock = mAllocateBlocks(1);
+        mFirstBlock->mem = heapMem;
+        mFirstBlock->size = size;
+        mFirstBlock->isFree = true;
+        mFirstBlock->next = nullptr;
+
+        mFreeBlock = mFirstBlock;
+    }
+
+    sizet HeapMemPool::Size() const noexcept
+    {
+        return mSize;
+    }
+
+    void HeapMemPool::Resize(const sizet size)
+    {
+        // todo implement this
+    }
+
+    void HeapMemPool::Reserve(const sizet size)
+    {
+        const sizet freeCount = FreeCount();
+        if (freeCount < size)
+        {
+            Resize(Size() - freeCount + size);
+        }
+    }
+}
