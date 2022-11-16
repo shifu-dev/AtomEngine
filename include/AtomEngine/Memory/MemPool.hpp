0d6835e3acd679f3310b819304d804ba9e83d140
Chetan Singh <csingh8476@gmail.com>
2022-11-16 14:07:47 +0530

    docs: documented MemPool and DynamicMemPool.

--- include/AtomEngine/Memory/MemPool.hpp
+++ include/AtomEngine/Memory/MemPool.hpp
@@ -4,22 +4,29 @@
 
 namespace Atom
 {
+    /// @brief MemPool is the base abstract type to manage memory pool.
     class MemPool : public virtual Allocator
     {
-        /// @return total memory managed in bytes 
+        /// @return Total count of memory units.
         mpublic virtual sizet Size() const noexcept abstract;
     };
 
+    /// @brief DynamicMemPool represents resizable memory pool.
     class DynamicMemPool : public virtual MemPool
     {
-        /// @brief shrinks the pool as much as possible
+        /// @brief Shrinks the pool as much as possible.
         mpublic virtual void Shrink() abstract;
 
-        /// @brief reserves specified memory
+        /// @brief Reserves specified memory.
+        /// @param size Minimum count of memory units to reserve.
+        /// 
+        /// After this call MemPool is gaurenteed to have at least size memory.
         mpublic virtual void Reserve(const sizet size) abstract;
 
-        /// @brief reserves specified memory, over currently reserved memory
-        /// does not checks for currently reserved memory
+        /// @brief Reserves specified memory, over currently reserved memory.
+        /// @param size Minimum count of memory units to reserve.
+        ///
+        /// @note ReserveMore is gaurenteed to allocate memory.
         mpublic virtual void ReserveMore(const sizet size) abstract;
     };
 }
