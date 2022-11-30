3060b813610827fe8f3df44323f7de381ffedc1f
Chetan Singh <csingh8476@gmail.com>
2022-11-30 15:53:54 +0530

    tests: added IConstIterable tests.

---
+++ tests/AtomEngine/Containers/TestIConstIterable.cpp
@@ -0,0 +1,38 @@
+#include "catch2/catch_all.hpp"
+#include "AtomEngine/Containers/IConstArray.hpp"
+#include "AtomEngine/Containers/ArrayView.hpp"
+
+using namespace Atom;
+
+TEST_CASE("IConstIterable Implementation")
+{
+    int rawArr[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    ArrayView<int> arr(rawArr, 10);
+
+    const IConstIterable<int> ref collection = arr;
+
+    SECTION("RangeBased ForLoop")
+    {
+        int i = 0;
+        for (auto value : citerate(collection))
+        {
+            CHECK(value == i);
+            i++;
+        }
+    }
+
+    SECTION("ForEachT")
+    {
+        int i = 0;
+        collection.ForEachT
+        (
+            [ref i] (const int ref value)
+            {
+                CHECK(value == i);
+                i++;
+            }
+        );
+    }
+
+    return;
+}
