3fa02820ea78dc8f3f9b5dce1b2c79d54ccebb72
Chetan Singh <csingh8476@gmail.com>
2022-12-01 15:29:15 +0530

    refactor: refactored code.

--- include/AtomEngine/Memory/Core.hpp
+++ include/AtomEngine/Memory/Core.hpp
@@ -3,29 +3,33 @@
 
 namespace Atom
 {
-    /// @brief Type used to represent unit memory block.
+    /// Type used to represent unit memory block.
+    /// 
     /// This type is gaurenteed to be compaitible with arithmetic operations (unlikes void).
     /// AtomEngine uses this type instead of typeless void.
     ///
     /// A single memt object is reffered as single memory unit.
     using memt = byte;
 
-    /// @brief Type representing ptr to unit memory block.
+    /// Type representing ptr to unit memory block.
+    /// 
     /// Recommended to use this instead of 'memt ptr' or 'memt*' for code readiblity.
     ///
     /// memptr is used to refer to a memory block.
     using memptr = memt ptr;
 
-    /// @brief Function to copy memory from one place to another.
-    /// @param dest Ptr to the destination of copy.
-    /// @param src Ptr to the source of copy.
+    /// Function to copy memory from one place to another.
+    /// 
+    /// @param dest Ptr to the destination to copy.
+    /// @param src Ptr to the source to copy.
     /// @param size Count of memory units to copy.
     inline void memcpy(memptr dest, memptr src, const sizet size) noexcept
     {
         std::memcpy(dest, src, size);
     }
 
-    /// @brief Writes @ref value at specified memory.
+    /// Writes \p{value} at specified memory.
+    /// 
     /// @param dest Ptr to the memory.
     /// @param value Value to write at memory.
     /// @param count Count of memory units to write.
