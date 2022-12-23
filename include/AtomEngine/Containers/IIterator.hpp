f657927b3f5e1bd61ad02f15c0ed9eea0218a061
Chetan Singh <csingh8476@gmail.com>
2022-12-23 18:46:00 +0530

    refactor: refactored code and doxygen.conf.

--- include/AtomEngine/Containers/IIterator.hpp
+++ include/AtomEngine/Containers/IIterator.hpp
@@ -3,10 +3,10 @@
 
 namespace Atom
 {
-    /// Object to iterate over container elements.
-    /// This Iterator does not modifies the elements.
-    ///
-    /// @tparam ElementT Type of element IConstIterator iterates over.
+    /// Object to iterate over a collection of elements.
+    /// This Iterator does not modifies the collection or its elements.
+    /// 
+    /// @tparam ElementT Type of element Iterator iterates over.
     template <typename ElementT>
     interface IConstIterator
     {
@@ -26,24 +26,24 @@ namespace Atom
         }
 
         /// Compares itself with other iterator.
-        ///
-        /// @param other Other IConstIterator to compare with.
+        /// 
+        /// @param other Other iterator to compare with.
         /// @return
         /// - = 0 if equal.
-        /// - > 0 if this == greater.
-        /// - < 0 if this == smaller.
+        /// - > 0 if this is greater.
+        /// - < 0 if this is smaller.
         /// 
         /// @note
         /// - This does not compares the element, but the Iterator.
         virtual int Compare(const IConstIterator& other) const noexcept abstract;
 
-        /// @returns Compare(const IConstIterator& other) == 0;
+        /// @returns Compare(other) == 0;
         bool operator == (const IConstIterator& other) const noexcept
         {
             return Compare(other) == 0;
         }
 
-        /// @returns Compare(const IConstIterator& other) != 0;
+        /// @returns Compare(other) != 0;
         bool operator != (const IConstIterator& other) const noexcept
         {
             return Compare(other) != 0;
@@ -51,28 +51,20 @@ namespace Atom
     };
 
     /// Object to iterate over container elements.
-    ///
+    /// 
     /// @tparam ElementT Type of element IIterator iterates over.
     template <typename ElementT>
     interface IIterator:
         public virtual IConstIterator<ElementT>
     {
-        using IConstIteratorT = IConstIterator<ElementT>;
-
-        using IConstIteratorT::Value;
-
         /// Get reference to current element.
         virtual ElementT& Value() noexcept abstract;
-
-        using IConstIteratorT::operator *;
 
         /// Get reference to current element.
         ElementT& operator * () noexcept
         {
             return Value();
         }
-
-        using IConstIteratorT::operator ->;
 
         /// Get pointer to current element.
         ElementT* operator -> () noexcept
